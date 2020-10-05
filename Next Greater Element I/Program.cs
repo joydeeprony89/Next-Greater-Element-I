d723165c726f42f353598b3cd52b44c8152c2ef0
Jaydeep Shil <ED8A@eurofins.com>
2020-10-05 21:40:45 +0530

    Add project files.

---
+++ Next Greater Element I/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Next_Greater_Element_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // https://leetcode.com/problems/next-greater-element-i/discuss/97595/Java-10-lines-linear-time-complexity-O(n)-with-explanation
+        public int[] NextGreaterElement(int[] nums1, int[] nums2)
+        {
+            Dictionary<int, int> pairs = new Dictionary<int, int>();
+            Stack<int> stack = new Stack<int>();
+            foreach (int num in nums2)
+            {
+                while (stack.Count > 0 && stack.Peek() < num)
+                {
+                    pairs.Add(stack.Peek(), num);
+                }
+                stack.Push(num);
+            }
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                nums1[i] = pairs.GetValueOrDefault(nums1[i], -1);
+            }
+            return nums1;
+        }
+    }
+}
